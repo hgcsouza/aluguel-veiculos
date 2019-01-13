5ef70922c0e0d7ccacbf96a07ebd77e41da86e1f
Henrique Souza <henriquegilbertosouza@gmail.com>
2019-01-13 16:13:06 -0200

    Start Project

---
+++ src/main/java/com/aluguel/AluguelVeiculoApplication.java
@@ -0,0 +1,14 @@
+package com.aluguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelVeiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelVeiculoApplication.class, args);
+	}
+
+}
+
